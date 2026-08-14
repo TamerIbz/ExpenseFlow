3c9bfc9b40a8a55b07ab835b6a36273399db66e2
Tam <tamibzz25@gmail.com>
2026-08-14 17:13:57 +0100

    initial

---
+++ ExpenseFlow/Models/Expense.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mvcPactice01.Models;
+
+public class Expense
+{
+    public int Id { get; set; }
+    public decimal Amount { get; set; }
+    [Required] public string? Description { get; set; }
+    
+    
+}
---
+++ ExpenseFlow/Models/ExpenseDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace mvcPactice01.Models;
+
+public class ExpenseDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    public ExpenseDbContext(DbContextOptions<ExpenseDbContext> options) : base(options)
+    {
+        
+    }
+}
